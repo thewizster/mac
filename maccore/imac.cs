475645fc64d211fec85a4db8dfa028f380559807
Raymond Brady <thewizster@gmail.com>
2016-12-22 10:19:27 -0600

    Inititial commit for NETStandard library

---
+++ maccore/imac.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Webextant.Security.Cryptography
+{
+    interface imac
+    {
+        bool IsValid(string message, string token);
+        bool KeyValIsValid(string token);
+        string GenerateToken(string message);
+        void AddKeyVal(string key, string val);
+        void ClearKeyVal();
+        string GenerateTokenFromKeyVal();
+    }
+}
